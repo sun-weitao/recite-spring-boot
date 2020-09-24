49c0173f4c5a2f74df8a352b9aaf9a9bb4c33544
sun <1457928205@qq.com>
2020-09-25 06:49:18 +0800

    first commit

---
+++ src/main/java/com/sun/recite/ReciteApplication.java
@@ -0,0 +1,13 @@
+package com.sun.recite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciteApplication.class, args);
+	}
+
+}
